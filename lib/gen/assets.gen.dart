0ecbee7ae1fdb691de95bb82c10a196202ca787c
akrnwl <moondo28@naver.com>
2025-05-16 02:54:17 +0900

    feat : add icons and rewrite pubspec.yaml

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,391 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/arrow_right.svg
+  SvgGenImage get arrowRight =>
+      const SvgGenImage('assets/icons/arrow_right.svg');
+
+  /// File path: assets/icons/badge_check_off.svg
+  SvgGenImage get badgeCheckOff =>
+      const SvgGenImage('assets/icons/badge_check_off.svg');
+
+  /// File path: assets/icons/badge_check_on.svg
+  SvgGenImage get badgeCheckOn =>
+      const SvgGenImage('assets/icons/badge_check_on.svg');
+
+  /// File path: assets/icons/bell.svg
+  SvgGenImage get bell => const SvgGenImage('assets/icons/bell.svg');
+
+  /// File path: assets/icons/boggle_icon.svg
+  SvgGenImage get boggleIcon =>
+      const SvgGenImage('assets/icons/boggle_icon.svg');
+
+  /// File path: assets/icons/bold.svg
+  SvgGenImage get bold => const SvgGenImage('assets/icons/bold.svg');
+
+  /// File path: assets/icons/check.svg
+  SvgGenImage get check => const SvgGenImage('assets/icons/check.svg');
+
+  /// File path: assets/icons/clock_off.svg
+  SvgGenImage get clockOff => const SvgGenImage('assets/icons/clock_off.svg');
+
+  /// File path: assets/icons/clock_on.svg
+  SvgGenImage get clockOn => const SvgGenImage('assets/icons/clock_on.svg');
+
+  /// File path: assets/icons/color_filter.svg
+  SvgGenImage get colorFilter =>
+      const SvgGenImage('assets/icons/color_filter.svg');
+
+  /// File path: assets/icons/community.svg
+  SvgGenImage get community => const SvgGenImage('assets/icons/community.svg');
+
+  /// File path: assets/icons/discuss_off.svg
+  SvgGenImage get discussOff =>
+      const SvgGenImage('assets/icons/discuss_off.svg');
+
+  /// File path: assets/icons/discuss_on.svg
+  SvgGenImage get discussOn => const SvgGenImage('assets/icons/discuss_on.svg');
+
+  /// File path: assets/icons/edit_pencil_off.svg
+  SvgGenImage get editPencilOff =>
+      const SvgGenImage('assets/icons/edit_pencil_off.svg');
+
+  /// File path: assets/icons/edit_pencil_on.svg
+  SvgGenImage get editPencilOn =>
+      const SvgGenImage('assets/icons/edit_pencil_on.svg');
+
+  /// File path: assets/icons/fire_flame_off.svg
+  SvgGenImage get fireFlameOff =>
+      const SvgGenImage('assets/icons/fire_flame_off.svg');
+
+  /// File path: assets/icons/fire_flame_on.svg
+  SvgGenImage get fireFlameOn =>
+      const SvgGenImage('assets/icons/fire_flame_on.svg');
+
+  /// File path: assets/icons/flower.svg
+  SvgGenImage get flower => const SvgGenImage('assets/icons/flower.svg');
+
+  /// File path: assets/icons/hashtag.svg
+  SvgGenImage get hashtag => const SvgGenImage('assets/icons/hashtag.svg');
+
+  /// File path: assets/icons/heading_big.svg
+  SvgGenImage get headingBig =>
+      const SvgGenImage('assets/icons/heading_big.svg');
+
+  /// File path: assets/icons/heading_small.svg
+  SvgGenImage get headingSmall =>
+      const SvgGenImage('assets/icons/heading_small.svg');
+
+  /// File path: assets/icons/heart_3_line_off.svg
+  SvgGenImage get heart3LineOff =>
+      const SvgGenImage('assets/icons/heart_3_line_off.svg');
+
+  /// File path: assets/icons/heart_3_line_on.svg
+  SvgGenImage get heart3LineOn =>
+      const SvgGenImage('assets/icons/heart_3_line_on.svg');
+
+  /// File path: assets/icons/home_simple_off.svg
+  SvgGenImage get homeSimpleOff =>
+      const SvgGenImage('assets/icons/home_simple_off.svg');
+
+  /// File path: assets/icons/home_simple_on.svg
+  SvgGenImage get homeSimpleOn =>
+      const SvgGenImage('assets/icons/home_simple_on.svg');
+
+  /// File path: assets/icons/ios_settings.svg
+  SvgGenImage get iosSettings =>
+      const SvgGenImage('assets/icons/ios_settings.svg');
+
+  /// File path: assets/icons/italic.svg
+  SvgGenImage get italic => const SvgGenImage('assets/icons/italic.svg');
+
+  /// File path: assets/icons/language.svg
+  SvgGenImage get language => const SvgGenImage('assets/icons/language.svg');
+
+  /// File path: assets/icons/link.svg
+  SvgGenImage get link => const SvgGenImage('assets/icons/link.svg');
+
+  /// File path: assets/icons/list.svg
+  SvgGenImage get list => const SvgGenImage('assets/icons/list.svg');
+
+  /// File path: assets/icons/lock.svg
+  SvgGenImage get lock => const SvgGenImage('assets/icons/lock.svg');
+
+  /// File path: assets/icons/media_image.svg
+  SvgGenImage get mediaImage =>
+      const SvgGenImage('assets/icons/media_image.svg');
+
+  /// File path: assets/icons/message_3_line_off.svg
+  SvgGenImage get message3LineOff =>
+      const SvgGenImage('assets/icons/message_3_line_off.svg');
+
+  /// File path: assets/icons/message_3_line_on.svg
+  SvgGenImage get message3LineOn =>
+      const SvgGenImage('assets/icons/message_3_line_on.svg');
+
+  /// File path: assets/icons/message_alert_off.svg
+  SvgGenImage get messageAlertOff =>
+      const SvgGenImage('assets/icons/message_alert_off.svg');
+
+  /// File path: assets/icons/message_alert_on.svg
+  SvgGenImage get messageAlertOn =>
+      const SvgGenImage('assets/icons/message_alert_on.svg');
+
+  /// File path: assets/icons/nav_arrow_down.svg
+  SvgGenImage get navArrowDown =>
+      const SvgGenImage('assets/icons/nav_arrow_down.svg');
+
+  /// File path: assets/icons/nav_arrow_left.svg
+  SvgGenImage get navArrowLeft =>
+      const SvgGenImage('assets/icons/nav_arrow_left.svg');
+
+  /// File path: assets/icons/nav_arrow_right.svg
+  SvgGenImage get navArrowRight =>
+      const SvgGenImage('assets/icons/nav_arrow_right.svg');
+
+  /// File path: assets/icons/nav_arrow_up.svg
+  SvgGenImage get navArrowUp =>
+      const SvgGenImage('assets/icons/nav_arrow_up.svg');
+
+  /// File path: assets/icons/plus_circle_off.svg
+  SvgGenImage get plusCircleOff =>
+      const SvgGenImage('assets/icons/plus_circle_off.svg');
+
+  /// File path: assets/icons/plus_circle_on.svg
+  SvgGenImage get plusCircleOn =>
+      const SvgGenImage('assets/icons/plus_circle_on.svg');
+
+  /// File path: assets/icons/search.svg
+  SvgGenImage get search => const SvgGenImage('assets/icons/search.svg');
+
+  /// File path: assets/icons/share_android_off.svg
+  SvgGenImage get shareAndroidOff =>
+      const SvgGenImage('assets/icons/share_android_off.svg');
+
+  /// File path: assets/icons/share_android_on.svg
+  SvgGenImage get shareAndroidOn =>
+      const SvgGenImage('assets/icons/share_android_on.svg');
+
+  /// File path: assets/icons/sparks_off.svg
+  SvgGenImage get sparksOff => const SvgGenImage('assets/icons/sparks_off.svg');
+
+  /// File path: assets/icons/sparks_on.svg
+  SvgGenImage get sparksOn => const SvgGenImage('assets/icons/sparks_on.svg');
+
+  /// File path: assets/icons/star_off.svg
+  SvgGenImage get starOff => const SvgGenImage('assets/icons/star_off.svg');
+
+  /// File path: assets/icons/star_on.svg
+  SvgGenImage get starOn => const SvgGenImage('assets/icons/star_on.svg');
+
+  /// File path: assets/icons/text.svg
+  SvgGenImage get text => const SvgGenImage('assets/icons/text.svg');
+
+  /// File path: assets/icons/trash_off.svg
+  SvgGenImage get trashOff => const SvgGenImage('assets/icons/trash_off.svg');
+
+  /// File path: assets/icons/trash_on.svg
+  SvgGenImage get trashOn => const SvgGenImage('assets/icons/trash_on.svg');
+
+  /// File path: assets/icons/underline.svg
+  SvgGenImage get underline => const SvgGenImage('assets/icons/underline.svg');
+
+  /// File path: assets/icons/user_circle_off.svg
+  SvgGenImage get userCircleOff =>
+      const SvgGenImage('assets/icons/user_circle_off.svg');
+
+  /// File path: assets/icons/user_circle_on.svg
+  SvgGenImage get userCircleOn =>
+      const SvgGenImage('assets/icons/user_circle_on.svg');
+
+  /// File path: assets/icons/view_grid_off.svg
+  SvgGenImage get viewGridOff =>
+      const SvgGenImage('assets/icons/view_grid_off.svg');
+
+  /// File path: assets/icons/view_grid_on.svg
+  SvgGenImage get viewGridOn =>
+      const SvgGenImage('assets/icons/view_grid_on.svg');
+
+  /// File path: assets/icons/warning_triangle_off.svg
+  SvgGenImage get warningTriangleOff =>
+      const SvgGenImage('assets/icons/warning_triangle_off.svg');
+
+  /// File path: assets/icons/warning_triangle_on.svg
+  SvgGenImage get warningTriangleOn =>
+      const SvgGenImage('assets/icons/warning_triangle_on.svg');
+
+  /// File path: assets/icons/white_flag_off.svg
+  SvgGenImage get whiteFlagOff =>
+      const SvgGenImage('assets/icons/white_flag_off.svg');
+
+  /// File path: assets/icons/white_flag_on.svg
+  SvgGenImage get whiteFlagOn =>
+      const SvgGenImage('assets/icons/white_flag_on.svg');
+
+  /// File path: assets/icons/xmark_circle_off.svg
+  SvgGenImage get xmarkCircleOff =>
+      const SvgGenImage('assets/icons/xmark_circle_off.svg');
+
+  /// File path: assets/icons/xmark_circle_on.svg
+  SvgGenImage get xmarkCircleOn =>
+      const SvgGenImage('assets/icons/xmark_circle_on.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+    arrowRight,
+    badgeCheckOff,
+    badgeCheckOn,
+    bell,
+    boggleIcon,
+    bold,
+    check,
+    clockOff,
+    clockOn,
+    colorFilter,
+    community,
+    discussOff,
+    discussOn,
+    editPencilOff,
+    editPencilOn,
+    fireFlameOff,
+    fireFlameOn,
+    flower,
+    hashtag,
+    headingBig,
+    headingSmall,
+    heart3LineOff,
+    heart3LineOn,
+    homeSimpleOff,
+    homeSimpleOn,
+    iosSettings,
+    italic,
+    language,
+    link,
+    list,
+    lock,
+    mediaImage,
+    message3LineOff,
+    message3LineOn,
+    messageAlertOff,
+    messageAlertOn,
+    navArrowDown,
+    navArrowLeft,
+    navArrowRight,
+    navArrowUp,
+    plusCircleOff,
+    plusCircleOn,
+    search,
+    shareAndroidOff,
+    shareAndroidOn,
+    sparksOff,
+    sparksOn,
+    starOff,
+    starOn,
+    text,
+    trashOff,
+    trashOn,
+    underline,
+    userCircleOff,
+    userCircleOn,
+    viewGridOff,
+    viewGridOn,
+    warningTriangleOff,
+    warningTriangleOn,
+    whiteFlagOff,
+    whiteFlagOn,
+    xmarkCircleOff,
+    xmarkCircleOn,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
